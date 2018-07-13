aaaa87fe9d4ddb2ca5041570816a81c11aab5aa1
Tanel Puhu <tanel@tanelpuhu.com>
2018-07-13 19:55:35 +0300

    init

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+
+	ps "github.com/mitchellh/go-ps"
+)
+
+func getStdin() []byte {
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Printf("error getting stdin: %v", err)
+	}
+	return data
+}
+
+func main() {
+	input := strings.TrimSpace(string(getStdin()))
+	var res []string
+	for _, pidtx := range strings.Split(input, "\n") {
+		if pidtx == "" {
+			continue
+		}
+		pid, err := strconv.Atoi(pidtx)
+		if err != nil {
+			fmt.Printf("this pid is not numeric: %s (%v)", pidtx, err)
+			continue
+		} else if pid > 0 {
+			p, err := ps.FindProcess(pid)
+			if err != nil {
+				fmt.Printf("error getting process %d info: %v", pid, err)
+			}
+			res = append(res, p.Executable())
+		}
+	}
+	if len(res) > 0 {
+		fmt.Printf(":%s", strings.Join(res, ","))
+	}
+}
